afed816ae84e6323794916fff4c011344f262a2a
Maged Faiz <magedfaiz98@gmail.com>
2023-09-07 20:56:06 +0200

    feat: add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Home Page
+    <main className=''>
+      <h1 className='text-4xl'>Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat hic
+        deserunt laudantium quidem magnam aliquam eligendi optio fugit nemo
+        incidunt, animi sapiente id eveniet repellat. Quam sequi debitis esse
+        atque.
+      </p>
     </main>
   );
 }
