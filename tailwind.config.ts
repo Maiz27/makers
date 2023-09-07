cc3fc1bbf57627af65e34c8d53faf3e160c9889f
Maged Faiz <magedfaiz98@gmail.com>
2023-09-07 20:16:20 +0200

    feat: add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,30 @@ const config: Config = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        makers: {
+          primary: '#FFDE59',
+
+          secondary: '#87f466',
+
+          accent: '#00AEDF',
+
+          neutral: '#E5EBE0',
+
+          'base-100': '#040401',
+
+          info: '#42aad7',
+
+          success: '#1d8145',
+
+          warning: '#e6c10a',
+
+          error: '#f54d82',
+        },
+      },
+    ],
+  },
 };
 export default config;
