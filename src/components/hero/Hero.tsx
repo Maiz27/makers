11239f4d6e2ab4386494736d061b5c7f17120037
Maged Faiz <magedfaiz98@gmail.com>
2023-09-08 21:20:30 +0200

    feat: add image slider

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,24 +1,42 @@
 import Image from 'next/image';
 import React from 'react';
-import hero1 from '/public/imgs/hero/3.jpg';
+import ImageSlider from './ImageSlider';
 
 const Hero = () => {
   return (
     <div className='h-[92vh] relative flex'>
       <div className='w-3/6 h-full'></div>
       <div className='w-[75%] h-full'>
-        <Image
-          src={hero1}
-          alt=''
-          className='h-full object-cover object-center'
-        />
+        <ImageSlider />
       </div>
       <div
         className='absolute inset-0 bg-base-100'
         style={{
           clipPath: 'polygon(0 0, 40% 0, 60% 100%, 0 100%)',
         }}
-      ></div>
+      >
+        <div className='h-full w-5/12 flex flex-col justify-center'>
+          <div className='z-20 ml-28 flex flex-col gap-8'>
+            <h1 className='text-4xl leading-normal'>
+              Redefining Possibilities in The Civil Engineering Industry
+            </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate placeat repellat sequi consectetur beatae. Molestiae
+              libero deleniti culpa minus distinctio quaerat quidem nesciunt,
+              nisi non commodi ipsum, ea rerum debitis.
+            </p>
+            <div className='flex items-center gap-4'>
+              <button className='btn normal-case btn-primary'>
+                Explore Services
+              </button>
+              <button className='btn normal-case btn-secondary'>
+                Previous Projects
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/hero/ImageSlider.tsx
@@ -0,0 +1,55 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+import hero1 from '/public/imgs/hero/1.jpg';
+import hero2 from '/public/imgs/hero/2.jpg';
+import hero3 from '/public/imgs/hero/3.jpg';
+
+const images = [
+  {
+    name: '1',
+    src: hero1,
+  },
+  {
+    name: '2',
+    src: hero2,
+  },
+  {
+    name: '3',
+    src: hero3,
+  },
+];
+
+const ImageSlider = () => {
+  const options = {
+    type: 'loop',
+    autoplay: true,
+    pauseOnHover: true,
+  };
+  return (
+    <Splide hasTrack={false} options={options} tag='section' className='h-full'>
+      <SplideTrack className='h-full'>
+        {images.map(({ name, src }) => {
+          return (
+            <SplideSlide key={name}>
+              <Image
+                src={src}
+                alt={name}
+                className='h-full object-cover object-center'
+              />
+            </SplideSlide>
+          );
+        })}
+      </SplideTrack>
+
+      <div className='splide__arrows hidden' />
+      <ul className='splide__pagination w-1/4' style={{ left: '50%' }}></ul>
+    </Splide>
+  );
+};
+
+export default ImageSlider;
