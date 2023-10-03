97a4e18e1995fbcdf674295bc0ca057bbcecc9fb
Maged Faiz <magedfaiz98@gmail.com>
2023-10-03 22:23:07 +0200

    feat: finalize project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/homeComp/hero/Hero';
 import About from '@/components/homeComp/About';
 import Services from '@/components/homeComp/services/Services';
+import Projects from '@/components/homeComp/projects/Projects';
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <About />
 
       <Services />
+
+      <Projects />
       <div className='h-screen'></div>
     </main>
   );
