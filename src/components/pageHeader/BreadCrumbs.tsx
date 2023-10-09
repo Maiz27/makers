0426fb2a9859bd3b701aa9630ac0417b78195435
Maged Faiz <magedfaiz98@gmail.com>
2023-10-09 10:51:45 +0200

    feat: disable current page breadcrumb link

--- src/components/pageHeader/BreadCrumbs.tsx
+++ src/components/pageHeader/BreadCrumbs.tsx
@@ -8,9 +8,11 @@ const BreadCrumbs = () => {
   const pathSegments = pathname.split('/').filter(Boolean);
 
   const breadcrumbs = pathSegments.map((segment, index) => {
+    const isLastSegment = index === pathSegments.length - 1;
     const breadcrumb = {
       label: segment.charAt(0).toUpperCase() + segment.slice(1), // Capitalize the first letter
       link: `/${pathSegments.slice(0, index + 1).join('/')}`, // Construct the breadcrumb link
+      isActive: isLastSegment && !segment.includes('?'), // Set isActive to true if it's the last segment and there are no query parameters
     };
     return breadcrumb;
   });
@@ -21,9 +23,13 @@ const BreadCrumbs = () => {
         <li>
           <Link href='/'>Home</Link>
         </li>
-        {breadcrumbs.map(({ link, label }) => (
+        {breadcrumbs.map(({ link, label, isActive }) => (
           <li key={link}>
-            {link ? <Link href={link}>{label}</Link> : <span>{label}</span>}
+            {!isActive ? (
+              <Link href={link}>{label}</Link>
+            ) : (
+              <span>{label}</span>
+            )}
           </li>
         ))}
       </ul>
